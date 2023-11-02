99166ed7fb89a6162a9d3ff80534f6ad9c83297a
aafonin <afonin_av@list.ru>
2023-11-02 11:20:47 +0300

    Added Task3

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,6 +1,9 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainClassTest extends MainClass {
 
     @Test
@@ -11,5 +14,12 @@ public class MainClassTest extends MainClass {
     @Test
     public void testGetClassNumber(){
         Assert.assertTrue("Number is less then 45!",getClassNumber() > 45);
+    }
+
+    @Test
+    public void testGetClassString(){
+        Pattern pattern = Pattern.compile("(?i:h)ello");
+        Matcher matcher = pattern.matcher(getClassString());
+        Assert.assertTrue("The string not contain 'Hello' or 'hello'!",matcher.find());
     }
 }
