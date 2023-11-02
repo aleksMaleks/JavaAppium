711ccea22439dadd3370ae5efe9a3119a1208cbf
aafonin <afonin_av@list.ru>
2023-11-02 10:15:05 +0300

    Added Task2

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -7,4 +7,9 @@ public class MainClassTest extends MainClass {
     public void checkNumber(){
         Assert.assertTrue("Number is not equal to 14!",getLocalNumber() == 14);
     }
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("Number is less then 45!",getClassNumber() > 45);
+    }
 }
